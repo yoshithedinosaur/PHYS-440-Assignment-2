6ebae780ea43ea51044b0a6cf44c804e667012ea
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-01-28 15:25:17 -0600

    Initial Commit

---
+++ Shared/Assignment_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment_2App.swift
+//  Shared
+//
+//  Created by Yoshinobu Fujikake on 1/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Assignment_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
