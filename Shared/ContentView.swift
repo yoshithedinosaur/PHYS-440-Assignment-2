78c9a053c0e32b5430c4cc22b550130829c34f7d
yoshithedinosaur <yfujikake@hawk.iit.edu>
2022-02-01 13:22:08 -0600

    updating inputs code

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,21 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var sinInput: String = "0"
+    
+    @State private var expandedSin: Double = 0.0
     
     var body: some View {
-        Text("Sin expansion")
-        TextField("Value to evaluate:", text: $sinInput, onCommit: {Task.init {await self.calculateSinSum()}})
-            .padding(.horizontal)
-            .frame(width: 100)
-            .padding(.top, 0)
-        Text("Sin(\(sinInput) )= \()")
+        Form {
+            Section {
+                TextField("Input to Sin", text: $expandedSin, onCommit: {Task.init {await self.calculateSinExpansion()}})
+            }
+        }
     }
-    
-    func calculateSinSum() async {
-        var displayVal: Double = sinSum(sinXIn: Double(sinInput)!)
-    }
+}
 
+func calculateSinExpansion() async {
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
